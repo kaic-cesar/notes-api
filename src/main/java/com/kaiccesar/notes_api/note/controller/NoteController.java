a9efdf7426f012e360053d3558f023e75b83b7f5
kaic-cesar <kaic_cesar@hotmail.com>
2026-09-11 15:28:28 -0300

    feat: implement note retrieval

--- src/main/java/com/kaiccesar/notes_api/note/controller/NoteController.java
+++ src/main/java/com/kaiccesar/notes_api/note/controller/NoteController.java
@@ -6,10 +6,9 @@ import com.kaiccesar.notes_api.note.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/notes")
@@ -17,6 +16,11 @@ public class NoteController {
 
     @Autowired
     NoteService service;
+
+    @GetMapping
+    public ResponseEntity<List<NoteResponseDTO>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
 
     @PostMapping
     public ResponseEntity<NoteResponseDTO> create(@RequestBody NoteRequestDTO requestDTO){
--- src/main/java/com/kaiccesar/notes_api/note/service/NoteService.java
+++ src/main/java/com/kaiccesar/notes_api/note/service/NoteService.java
@@ -8,12 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class NoteService {
 
     @Autowired
     NoteRepository repository;
+
+    public List<NoteResponseDTO> findAll(){
+        return repository.findAll()
+                .stream()
+                .map(noteModel -> new NoteResponseDTO(
+                        noteModel.getId(),
+                        noteModel.getTitle(),
+                        noteModel.getContent(),
+                        noteModel.getCreateAt(),
+                        noteModel.getUpdateAt()
+                ))
+                .toList();
+    }
 
     public NoteResponseDTO create(NoteRequestDTO requestDTO){
         NoteModel note = new NoteModel();
